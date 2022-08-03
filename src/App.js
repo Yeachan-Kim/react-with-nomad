df5317ccd53b5be89892a8a22a284c0d1162a6f8
13number <52204621+13number@users.noreply.github.com>
2022-08-03 23:08:25 +0900

    asd

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Detail from './routes/Detail';
+import Home from './routes/Home';
+function App() {
+  return (
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/movie/:id' element={<Detail/>}></Route>
+    </Routes>
+  </BrowserRouter>
+  )}
+export default App;
